3748bbfeffecb2c840ee12d1b189391b7b4ef69c
Fabrice Piedanna <rs45959@gmail.com>
2025-09-04 16:23:22 +0200

    Add nativeWind

---
+++ app/(tabs)/testnativewind.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function testnativewind() {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-2xl font-extrabold text-white border-[10px] border-white p-6 rotate-[52deg]">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
